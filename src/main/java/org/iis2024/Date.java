6d0ad2698dac9ce8eca2349f408e3c533d7422dd
frank <frarosare@gmail.com>
2025-03-13 14:13:46 +0100

    Cambio de clase Date

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -25,6 +25,7 @@ public class Date {
    *
    * @return The day of the month (1-31).
    */
+
   public int getDay() {
     return day;
   }
@@ -60,7 +61,11 @@ public class Date {
     this.month = month;
     this.year = year;
   }
-
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
+  }
   /**
    * Validates the date according to the predefined rules: the year must be within 1900 to 2050, the
    * month within 1 to 12, and the day valid within the given month and year, considering leap
